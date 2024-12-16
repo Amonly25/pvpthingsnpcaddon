b4e495edf4c652f6c0e0501540200f381fcbc6c9
Luis Martinez <amonly97@gmail.com>
2024-12-16 20:23:16 -0300

    -

--- src/main/java/com/ar/askgaming/pvpthingsnpcaddon/CheckNpcTask.java
+++ src/main/java/com/ar/askgaming/pvpthingsnpcaddon/CheckNpcTask.java
@@ -1,7 +1,5 @@
 package com.ar.askgaming.pvpthingsnpcaddon;
 
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
